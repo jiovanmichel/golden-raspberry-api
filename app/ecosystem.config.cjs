08c58b54430ae38a0a6e67ff6b862b2b8a810f82
Jiovan <jiiovan@unochapeco.edu.br>
2024-11-12 01:55:16 -0300

    Limpeza de conexão e consultas sqlite antes do sequilize.

--- app/ecosystem.config.cjs
+++ app/ecosystem.config.cjs
@@ -5,14 +5,9 @@ module.exports = {
         log_date_format: 'DD/MMM HH:mm:ss',
         watch: false,
         ignore_watch: ['node_modules', '*.log'],
-        env: {
-            NODE_ENV: "development",
-            watch: true, // TODO mudar para tru quando desenvolver
-            ignore_watch: ['node_modules', 'err.log', '*.log']
-        },
         env_development: {
             NODE_ENV: "development",
-            watch: true, // TODO mudar para tru quando desenvolver
+            watch: false, // TODO mudar para tru quando desenvolver
             ignore_watch: ['node_modules', 'err.log', '*.log']
         },
         env_production: {
